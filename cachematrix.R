07193afb94d7d7e7e84796fba7d4b376b0d92a41
Michael Giessing <michael.giessing@healthcatalyst.com>
2016-12-30 09:37:17 -0700

    Got Working Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Given an invertible matrix these functions
+## create method to store and update the inverse
+## of the matrix with it.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<-y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invrs) inv<<- invrs
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
+  
 }
 
 
@@ -12,4 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setinv(inv)
+  }
